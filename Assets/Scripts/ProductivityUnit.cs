e13271f6f23123fed48fd721e7ae7871ffcf12ce
Petr Hampl <2007hampl@seznam.cz>
2021-10-28 13:52:54 +0200

    done Abstraction, Inheritance and polymorphism

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -9,6 +9,33 @@ public class ProductivityUnit : Unit
     
     protected override void BuildingInRange()
     {
-      
+        if (m_CurrentPile ==null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+    }
+    void ResetProductivity()
+    {
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
+    }
+    public override void GoTo(Building target)
+    {
+        ResetProductivity(); 
+        base.GoTo(target); 
+    }
+    public override void GoTo(Vector3 position)
+    {
+        ResetProductivity(); 
+        base.GoTo(position); 
     }
 }
