6e528a206f9a0b27abcd897a207bc4b214160fd5
Petr Hampl <2007hampl@seznam.cz>
2021-10-28 13:35:58 +0200

    initial commit

---
+++ Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductivityUnit : Unit
+{
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplier = 2;
+    
+    protected override void BuildingInRange()
+    {
+      
+    }
+}
